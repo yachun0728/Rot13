66313681c78899f65acd05b17a6c59ad1ea8bd95
Angel Lu <angel.lu86@titansoft.com.sg>
2018-03-13 18:43:19 +0800

    Pass the test

---
+++ Rot13/Rot13/Rot13.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rot13
+{
+    public class Kata
+    {
+        static Dictionary<string, string> _CharMapping = new Dictionary<string, string>()
+        {
+            {"A", "N" },{"B", "O"},{"C", "P"},{"D", "Q"},{"E", "R"},{"F", "S"},{"G", "T"},{"H", "U"},
+            {"I", "V"},{"J", "W"},{"K", "X"},{"L", "Y"},{"M", "Z"},{"N", "A"},{"O", "B"},{"P", "C"},
+            {"Q", "D"},{"R", "E"},{"S", "F"},{"T", "G"},{"U", "H"},{"V", "I"},{"W", "J"},{"X", "K"},
+            {"Y", "L"},{"Z", "M"}
+
+        };
+        public static string Rot13(string input)
+        {
+            var inputArray = input.ToCharArray();
+            string output = string.Empty;
+            foreach (var a in inputArray)
+            {
+                if (_CharMapping.ContainsKey(a.ToString()) || _CharMapping.ContainsKey(a.ToString().ToUpper()))
+                {
+                    if (_CharMapping.ContainsKey(a.ToString()))
+                    {
+                        output += _CharMapping[a.ToString()];
+                    }
+                    else
+                    {
+                        output += _CharMapping[a.ToString().ToUpper()].ToLower();
+                    }
+                }
+                else
+                {
+                    output += a.ToString();
+                }
+            }
+            return output;
+        }
+        
+    }
+}
---
+++ Rot13/Rot13Tests/KataTests.cs
@@ -0,0 +1,27 @@
+﻿using Rot13;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Rot13.Tests
+{
+    [TestFixture]
+    public class SystemTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("ROT13 example.", Kata.Rot13("EBG13 rknzcyr."));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("This is my first ROT13 excercise!", Kata.Rot13("Guvf vf zl svefg EBG13 rkprepvfr!"));
+        }
+    }
+}
